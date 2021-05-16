b31dd11fc3074ddd62a1254d8b024bd1495c18cc
adi518 <adi.s@openweb.com>
2021-05-17 00:30:20 +0300

    update deps

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
-const { resolveExternals } = require('node-externals')
+const { resolveExternals } = require("monorepo-utils");
 
 module.exports = {
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     // https://stackoverflow.com/a/45820235/4106263
     config.externals(
-      resolveExternals({ importType: 'umd', allowlist: [/prismjs/] })
-    )
+      resolveExternals({ importType: "umd", allowlist: [/prismjs/] })
+    );
   },
   pluginOptions: {
     jestSerializer: {
@@ -13,4 +13,4 @@ module.exports = {
     },
   },
   css: { extract: false },
-}
+};
